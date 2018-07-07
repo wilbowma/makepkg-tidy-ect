fd74ea345fef3bbbac745b6e98daf8b8d78f1cbc
William J. Bowman <wjb@williamjbowman.com>
2018-07-07 15:37:31 -0400

    Added tidy script and documentation; roughly tested

--- README.md
+++ README.md
@@ -3,3 +3,25 @@ makepkg-tidy-ect
 
 A `libmakepkg` tidy extension that runs `ect` on all PNGs, JPEGs, ZIPs, and
 GZIPs in an installed package, optimizing their size.
+
+## Installation
+Drop `tidy_ect.sh` into `/usr/share/makepkg/tidy`.
+Or, preferably, install from `makepkg-tidy-ect` from the AUR.
+
+To enable it during a `makepkg` run, see below.
+
+## Usage
+Add one or more of the following to the `OPTIONS` array used by `makepkg`.
+This can be done on a per-`PKGBUILD` basis, or in `/etc/makepkg.conf`.
+
+- `ect`: enables all of the following options.
+- `ect_png`: compresses all PNGs found in the `pkgdir`.
+- `ect_jpg`: compresses all JPGs found in the `pkgdir`.
+- `ect_zip`: compresses all ZIPs found in the `pkgdir`.
+- `ect_gzip`: compresses all GZIPs found in the `pkgdir`.
+
+For example, my `OPTIONS` array in `/etc/makepkg.conf` is the following:
+
+```
+OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge ect !debug)
+```
---
+++ tidy_ect.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+#
+#   tidy_ect.sh - Losslessly and efficiently compress files with ect.
+#
+#   Copyright (c) 2018 William J. Bowman <wjb@williamjbowman.com>
+
+
+[[ -n "$LIBMAKEPKG_TIDY_ECT_SH" ]] && return
+LIBMAKEPKG_TIDY_ECT_SH=1
+
+LIBRARY=${LIBRARY:-'/usr/share/makepkg'}
+
+source "$LIBRARY/util/message.sh"
+source "$LIBRARY/util/option.sh"
+
+
+packaging_options+=('ect' 'ect_png' 'ect_jpg' 'ect_zip' 'ect_jar' 'ect_gzip')
+tidy_modify+=('tidy_ect')
+
+ECT_OPTS="-9 -keep --strict --mt-deflate"
+
+tidy_ect() {
+	if check_option "ect" "y"; then
+    msg2 "$(gettext "Compressing files with ect...")"
+    options+=('ect_png' 'ect_jpg' 'ect_zip' 'ect_jar' 'ect_gzip')
+  fi
+
+	if check_option "ect_png" "y"; then
+		msg2 "$(gettext "Compressing PNGs ....")"
+    find . -depth -type f -iname "*.png" -print0 | xargs -0 -r ect $ECT_OPTS 2>/dev/null
+  fi
+
+	if check_option "ect_jpg" "y"; then
+    msg2 "$(gettext "Compressing JPGs ....")"
+    find . -depth -type f -iname "*.jpg" -or -iname "*.jpeg" -print0 | xargs -0 -r ect -progresive $ECT_OPTS 2>/dev/null
+  fi
+
+	if check_option "ect_zip" "y"; then
+		msg2 "$(gettext "Compressing ZIPs ....")"
+    find . -depth -type f -iname "*.zip" -print0 | xargs -0 -r ect -zip $ECT_OPTS 2>/dev/null
+  fi
+
+#	if check_option "ect_jar" "y"; then
+#    msg2 "$(gettext "Compressing JARs ....")"
+#    #find . -depth -type f -iname "*.jar" -print0 | xargs -0 -r ect -zip $ECT_OPTS 2>/dev/null
+#  fi
+
+  if check_option "ect_gzip" "y"; then
+		msg2 "$(gettext "Compressing GZIPs ....")"
+    find . -depth -type f -iname "*.gzip" -or -iname "*.gz" -print0 | xargs -0 -r ect -gzip $ECT_OPTS 2>/dev/null
+  fi
+}
