8ada668279a4949fc98e3fb5605a49e2911f1dc5
William J. Bowman <wjb@williamjbowman.com>
2018-07-07 16:10:49 -0400

    Removed overly clever use of xargs
    In theory, this could let ect parallelize things.
    In practice, it requires non-standard GNU options, and, doesn't seem to work.

--- tidy_ect.sh
+++ tidy_ect.sh
@@ -27,17 +27,20 @@ tidy_ect() {
 
 	if check_option "ect_png" "y"; then
 		msg2 "$(gettext "Compressing PNGs ....")"
-    find . -depth -type f -iname "*.png" -print0 | xargs -0 -r ect $ECT_OPTS 2>/dev/null
+    #find . -depth -type f -iname "*.png" -print0 | xargs -0 -r ect $ECT_OPTS
+    find . -depth -type f -iname "*.png" -exec ect $ECT_OPTS '{}' \;
   fi
 
 	if check_option "ect_jpg" "y"; then
     msg2 "$(gettext "Compressing JPGs ....")"
-    find . -depth -type f -iname "*.jpg" -or -iname "*.jpeg" -print0 | xargs -0 -r ect -progresive $ECT_OPTS 2>/dev/null
+    #find . -depth -type f -iname "*.jpg" -or -iname "*.jpeg" -print0 | xargs -0 -r ect -progresive $ECT_OPTS
+    find . -depth -type f -iname "*.jpg" -or -iname "*.jpeg" -exec ect -progresive $ECT_OPTS '{}' \;
   fi
 
 	if check_option "ect_zip" "y"; then
 		msg2 "$(gettext "Compressing ZIPs ....")"
-    find . -depth -type f -iname "*.zip" -print0 | xargs -0 -r ect -zip $ECT_OPTS 2>/dev/null
+    #find . -depth -type f -iname "*.zip" -print0 | xargs -0 -r ect -zip $ECT_OPTS 2>/dev/null
+    find . -depth -type f -iname "*.zip" -exec ect -zip $ECT_OPTS '{}' \; 2>/dev/null
   fi
 
 #	if check_option "ect_jar" "y"; then
@@ -47,6 +50,7 @@ tidy_ect() {
 
   if check_option "ect_gzip" "y"; then
 		msg2 "$(gettext "Compressing GZIPs ....")"
-    find . -depth -type f -iname "*.gzip" -or -iname "*.gz" -print0 | xargs -0 -r ect -gzip $ECT_OPTS 2>/dev/null
+    #find . -depth -type f -iname "*.gzip" -or -iname "*.gz" -print0 | xargs -0 -r ect -gzip $ECT_OPTS 2>/dev/null
+    find . -depth -type f -iname "*.gzip" -or -iname "*.gz" -exec ect -gzip $ECT_OPTS '{}' \; 2>/dev/null
   fi
 }
